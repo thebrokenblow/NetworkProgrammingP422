54d9ff3302ede5fb8635a67b76d4952b64bee1b6
Артём Красов <artkrasov@gmail.com>
2026-07-16 18:57:52 +0300

    Добавил клиента udp и сервер udp

---
+++ ClietnUdp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Клиентское приложение");
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+var serverEndPoint = new IPEndPoint(IPAddress.Loopback, 11000);
+
+var clietnIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    var receiveBuffer = new byte[65507];
+
+    Console.WriteLine("Введите данные: ");
+    var message = Console.ReadLine();
+    var sendBuffer = Encoding.UTF8.GetBytes(message);
+
+    await socket.SendToAsync(
+          sendBuffer,
+          SocketFlags.None,
+          serverEndPoint);
+
+    var result = await socket.ReceiveFromAsync(
+          receiveBuffer,
+          SocketFlags.None,
+          clietnIpEndPoint);
+
+    string response = Encoding.UTF8.GetString(receiveBuffer, 0, result.ReceivedBytes);
+    Console.WriteLine($"Ответ от сервера: {response}");
+}
---
+++ ServerUdP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Серверное приложение");
+
+const int BufferSize = 1024;
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+socket.Bind(new IPEndPoint(IPAddress.Loopback, 11000));
+
+
+while (true)
+{
+    var clietnIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    var buffer = new byte[BufferSize];
+    var socketReceiveFromResult = await socket.ReceiveFromAsync(buffer, clietnIpEndPoint);
+
+    await ProcessMessageAsync(socket, buffer, socketReceiveFromResult.ReceivedBytes, socketReceiveFromResult.RemoteEndPoint);
+}
+
+static async Task ProcessMessageAsync(Socket socket, byte[] buffer, int receivedBytes, EndPoint clietnIpEndPoint)
+{
+    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+    Console.WriteLine($"Полученные данные: {receivedMessage}");
+
+
+    var message = Encoding.UTF8.GetBytes($"Получил сообщение: {receivedMessage}");
+
+    await socket.SendToAsync(
+         message,
+         SocketFlags.None,
+         clietnIpEndPoint);
+}
